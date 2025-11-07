203e8c64b02297968367c6d294354fc821e3b32a
Antooon32 <robotkartoshka@gmail.com>
2025-11-07 20:40:19 +0200

    loader 2

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,16 @@
-window.addEventListener('load', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const preloadElement = document.querySelector('.preload');
+    const body = document.body;
 
     if (preloadElement) {
         preloadElement.classList.add('preload__animation');
+        preloadElement.addEventListener('animationend', function handler() {
+            body.classList.remove('no-scroll');
+            preloadElement.removeEventListener('animationend', handler);
+
+        }, { once: true });
+
+    } else {
+        body.classList.remove('no-scroll');
     }
 });
